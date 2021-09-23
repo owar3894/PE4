9129d369064e0fea0ac8c5b19b599511f3060484
jackjj1125 <77815395+jackjj1125@users.noreply.github.com>
2021-09-23 15:10:08 -0600

    Update TicTacToe.cpp
    step 3. for Create Board func.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 
 
-
-CreateBoard()
+void CreateBoard() //function to create board
 {
+    int row = 3; //setting number of rows to 3 for our 3x3 board
+    int col = 3; //setting number of cols to 3 for our 3x3 board
 
+    std::vector<std::vector<int> > vec(row, std::vector<int> (col, 0));
+    //init our 2d vector here
+  
 }
 
 
 int main()
 {
-
+    CreateBoard();
 
 }
