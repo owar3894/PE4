bd4ada2dd834c4849516b3354aca8d6eec57f0f4
owar3894 <owar3894@colorado.edu>
2021-09-24 15:46:00 -0600

    adding display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,19 +2,30 @@
 #include <vector>
 
 
-void CreateBoard() //function to create board
+std::vector<std::vector<int> > CreateBoard() //function to create board
 {
     int row = 3; //setting number of rows to 3 for our 3x3 board
     int col = 3; //setting number of cols to 3 for our 3x3 board
 
     std::vector<std::vector<int> > vec(row, std::vector<int> (col, 0));
-    //init our 2d vector here
-  
+    //init our 2d vector here 
+    return vec;
+}
+
+void DisplayBoard(std::vector<std::vector<int> > vec){
+    for(int i = 0; i < vec.size(); i++){
+        for(int j = 0; j < vec.size(); j++){
+            std::cout << vec[i][j];
+        }
+        std::cout << std::endl;
+       
+    }
 }
 
 
-int main()
-{
-    CreateBoard();
+int main(){
 
+    std::vector<std::vector<int> > vec = CreateBoard();
+
+    DisplayBoard(vec);
 }
